3aed4f97e4fc1224ac7136a6b4cb910de2d7d0dc
Ashish <ashish@Ashish-MacBook-Air.local>
2016-03-10 02:57:40 +0530

    Update changes

--- js/analyzer.js
+++ js/analyzer.js
@@ -13,5 +13,5 @@ var trackingId='UA-74926903-1';
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', trackingId, 'auto');
-ga('create', 'event', 'Outbound Link', 'click', event.target.href);
+ga('send', 'event', 'Button', 'Click', 'Download');
 ga('send', 'pageview');
