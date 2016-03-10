16df00ad44da4578b563d809adebbfbf15ead4c8
Ashish <ashish@Ashish-MacBook-Air.local>
2016-03-10 06:23:44 +0530

    Push

--- js/analyzer.js
+++ js/analyzer.js
@@ -1,21 +1,18 @@
 var trackingId = 'UA-74926903-1';
 
 (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', trackingId, 'auto');
-
-var image_variations = [
-    'header.jpg',
-    '56H.jpg'
-]
-
-var variation = cxApi.chooseVariation(2);
-
-window.onload = function() {
-    exp_image = document.getElementById('img-responsive');
-    exp_image.src = image_variations[variation];
-}
 
 ga('send', 'pageview');
 
