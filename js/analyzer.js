1f9f00cbf9cf889ed0da3b88be7573396c927b2e
Ashish <ashish@Ashish-MacBook-Air.local>
2016-03-10 05:53:15 +0530

    Minor fixes

--- js/analyzer.js
+++ js/analyzer.js
@@ -1,35 +1,24 @@
-var trackingId='UA-74926903-1';
+var trackingId = 'UA-74926903-1';
 
 (function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', trackingId, 'auto');
 
 var image_variations = [
-  'header.jpg',
-  '56H.jpg'
+    'header.jpg',
+    '56H.jpg'
 ]
 
 var variation = cxApi.chooseVariation();
 
-window.onload = function(){
-exp_image = document.getElementById('img-responsive');
-exp_image.src = image_variations[variation];
+window.onload = function() {
+    exp_image = document.getElementById('img-responsive');
+    exp_image.src = image_variations[variation];
 }
 
 ga('send', 'pageview');
 
-$(".btn").click(function(){
+$(".btn").click(function() {
     ga('send', 'event', 'button', 'click', 'Download');
 });
-
-
