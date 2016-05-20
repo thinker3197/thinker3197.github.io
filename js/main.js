04d27e96b7aa500f207ec5fb993d930a660078fd
thinker3197 <ashishgupta.3197@gmail.com>
2016-05-20 23:19:23 +0530

    Terminal input taken

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-var terminal = function() {
-    var requiredText = '>> ';
-    $('.terminal-input').on('input', function() {
-        if (String($(this).val()).indexOf(requiredText) == -1) {
-            $(this).val(requiredText);
+function terminal(e, input) {
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 13) {
+        var terminalDiv = $('.terminal');
+        var inputVal = $('.terminal-input').val();
+        if (inputVal == 'ls') {
+            terminalDiv.append('<p>projects &nbsp;&nbsp;resume.pdf &nbsp;&nbsp;game.app</p>');
         }
-    });
-}
+        terminalDiv.append('<p class="console"> ~ $ashish>> <input type = "text" class = "terminal-input" onKeyPress = "terminal(event, this)" autofocus></p>');
+        }
+    }
 
-$(document).ready(function() {
-    $('.fa').on('click', function() {
-        if ($('.intro').hasClass('clicked')) {
-            $('.intro').removeClass('clicked');
-        } else {
-            $('.intro').addClass('clicked');
-        }
+    $(document).ready(function() {
+        $('.fa').on('click', function() {
+            if ($('.intro').hasClass('clicked')) {
+                $('.intro').removeClass('clicked');
+            } else {
+                $('.intro').addClass('clicked');
+            }
+        });
     });
-    terminal();
-});
