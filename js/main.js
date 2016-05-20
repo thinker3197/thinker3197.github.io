7278140a303599da4f146f0babc56219b5b93d9e
thinker3197 <ashishgupta.3197@gmail.com>
2016-05-20 17:27:05 +0530

    Prevent backspace default

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
+var terminal = function() {
+    var requiredText = '>> ';
+    $('.terminal-input').on('input', function() {
+        if (String($(this).val()).indexOf(requiredText) == -1) {
+            $(this).val(requiredText);
+        }
+    });
+}
+
 $(document).ready(function() {
     $('.fa').on('click', function() {
-    	if($('.intro').hasClass('clicked')){
-        	$('.intro').removeClass('clicked');
-    	}
-    	else
-    	{
-    		$('.intro').addClass('clicked');
-    	}
+        if ($('.intro').hasClass('clicked')) {
+            $('.intro').removeClass('clicked');
+        } else {
+            $('.intro').addClass('clicked');
+        }
     });
+    terminal();
 });
