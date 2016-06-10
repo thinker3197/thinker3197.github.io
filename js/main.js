8a68323ad6c4ced8f241d0a4d30f9d633a6fba84
thinker3197 <ashishgupta.3197@gmail.com>
2016-06-10 13:32:36 +0530

    Enhancements terminal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var flag = 0;
+
 function terminal(e, input) {
 
     var code = (e.keyCode ? e.keyCode : e.which);
@@ -7,19 +9,37 @@ function terminal(e, input) {
         var inputVal = $('.terminal-input').val();
 
         $('.console').last().remove();
-        terminalDiv.append('~ $ashish >> ' + inputVal);
-        if (inputVal == 'ls') {
+        if(flag == 0) {
+            terminalDiv.append('~ $ashish >> ' + inputVal);
+        }
+        else {
+            terminalDiv.append('~/projects $ashish >> ' + inputVal);
+        }
+        if (inputVal == 'ls' && flag == 0) {
             terminalDiv.append('<p>projects &nbsp;&nbsp;resume.pdf &nbsp;&nbsp;game.app</p>');
-        } else if (inputVal == 'cat resume.pdf') {
-            window.location.href='https://drive.google.com/file/d/0B3OeNoTC80mGaTBUbC1ESG5vN1k/view?usp=sharing';
+        } else if (inputVal == 'ls' && flag == 1) {
+            terminalDiv.append('<p>WikiSearch &nbsp;&nbsp;Clippy &nbsp;&nbsp;ink &nbsp;&nbsp;Erectus</p>');
+        } else if (inputVal == 'open resume.pdf' && flag == 0) {
+            window.location.hre = 'https://drive.google.com/file/d/0B3OeNoTC80mGaTBUbC1ESG5vN1k/view?usp=sharing';
         } else if (inputVal == 'cd projects') {
-            terminalDiv.append('<p>WikiSearch &nbsp;&nbsp;Clippy &nbsp;&nbsp;Erectus</p>');
-        } else if (inputVal == 'cat game.app' || inputVal == 'cd WikiSearch' || inputVal == 'cd Erectus' || inputVal == 'Clippy') {
-            terminalDiv.append('<p>Will be added soon!</p>');
+            terminalDiv.append('<p></p>');
+            flag = 1;
+        } else if ((inputVal == 'cd WikiSearch' ||  inputVal == 'cd ink') && flag == 1) {
+            window.location.href = 'http://github.com/thinker3197/' + inputVal.slice(3, inputVal.length);
+        } else if ((inputVal == 'cd Erectus' || inputVal == 'cd Clippy') && flag == 1) {
+            window.location.href = 'http://github.com/The-Turing-Machine/' + inputVal.slice(3, inputVal.length);
+        } else if (inputVal == 'cd ..' && flag == 1) {
+            terminalDiv.append('<p></p>');
+            flag = 0;
         } else {
             terminalDiv.append('<p>Enter something valid!</p>');
         }
-        terminalDiv.append('<p class="console"> ~ $ashish >> <input type = "text" class = "terminal-input" onKeyPress = "terminal(event, this)" autofocus></p>');
+        if(flag == 0) {
+            terminalDiv.append('<p class="console"> ~ $ashish >> <input type = "text" class = "terminal-input" onKeyPress = "terminal(event, this)" autofocus></p>');
+        }
+        else {
+            terminalDiv.append('<p class="console"> ~/projects $ashish >> <input type = "text" class = "terminal-input" onKeyPress = "terminal(event, this)" autofocus></p>');
+        }
         $(".terminal-input").focus();
     }
 }
