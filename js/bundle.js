54067745be3688e0d089adfacb9a545624c1b23e
thinker3197 <ashishgupta.3197@gmail.com>
2016-10-26 16:48:02 +0530

    Update class structure

--- js/bundle.js
+++ js/bundle.js
@@ -5,16 +5,25 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var fadeOut = function fadeOut(element) {
-  var op = 1;
+  var opacity = 1;
   var timer = setInterval(function () {
-    if (op <= 0.1) {
+    if (opacity <= 0.1) {
       clearInterval(timer);
       element.style.display = 'none';
     }
-    element.style.opacity = op;
-    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-    op -= op * 0.1;
+    element.style.opacity = opacity;
+    element.style.filter = 'alpha(opacity=' + opacity * 100 + ")";
+    opacity -= opacity * 0.1;
   }, 50);
+};
+
+var createEl = function createEl(el, className, text) {
+  var elem = document.createElement(el);
+
+  elem.innerHTML = text;
+  elem.setAttribute('class', className);
+
+  return elem;
 };
 
 var Terminal = function () {
@@ -24,10 +33,15 @@ var Terminal = function () {
     this.cover = document.querySelector('#cover');
     this.terminalMenu = document.querySelector('i');
     this.intro = document.querySelector('.intro');
+    this.terminal = document.querySelector('.terminal');
+    this.terminalInput = document.querySelector('.terminal-input');
+    this.console = document.querySelector('.console');
 
-    fadeOut(this.cover);
+    this.root = true;
 
     this.attachListeners();
+
+    fadeOut(this.cover);
   }
 
   _createClass(Terminal, [{
@@ -38,6 +52,37 @@ var Terminal = function () {
       this.terminalMenu.onclick = function () {
         _this.intro.classList.toggle('clicked');
       };
+      this.terminalInput.onkeypress = function (e) {
+        if ((e.keyCode ? e.keyCode : e.which) === 13) {
+          _this.getOutput(_this.terminalInput.value);
+        }
+      };
+    }
+  }, {
+    key: 'getOutput',
+    value: function getOutput(value) {
+      this.terminal.removeChild(this.console);
+
+      if (this.root) {
+        this.terminal.appendChild(createEl('P', '', '~ $ashish >> ' + value));
+      } else {
+        this.terminal.appendChild(createEl('P', '', '~/projects $ashish >> ' + value));
+      }
+
+      switch (value) {
+        case 'ls':
+          if (this.root) {
+            this.terminal.appendChild(createEl('P', '', 'projects &nbsp;&nbsp;resume.pdf &nbsp;&nbsp;me.jpg'));
+          } else {
+            this.terminalMenu.appendChild(createEl('P', '', 'Progressively &nbsp;&nbsp;Clippy &nbsp;&nbsp;ink &nbsp;&nbsp;Erectus'));
+          }
+      }
+
+      if (this.root) {
+        this.terminal.appendChild(createEl('P', 'console', '~ $ashish >> <input type="text" class="terminal-input" autofocus>'));
+      } else {
+        this.terminal.appendChild(createEl('P', 'console', '~/projects $ashish >> <input type="text" class="terminal-input" autofocus>'));
+      }
     }
   }]);
 
--- src/main.js
+++ src/main.js
@@ -1,32 +1,76 @@
 const fadeOut = (element) => {
-  var op = 1;
-  var timer = setInterval(function() {
-    if (op <= 0.1) {
+  let opacity = 1;
+  let timer = setInterval(function() {
+    if (opacity <= 0.1) {
       clearInterval(timer);
       element.style.display = 'none';
     }
-    element.style.opacity = op;
-    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-    op -= op * 0.1;
+    element.style.opacity = opacity;
+    element.style.filter = 'alpha(opacity=' + opacity * 100 + ")";
+    opacity -= opacity * 0.1;
   }, 50);
 };
+
+const createEl = (el, className, text) => {
+  const elem = document.createElement(el);
+
+  elem.innerHTML = text;
+  elem.setAttribute('class', className);
+
+  return elem;
+}
 
 class Terminal {
   constructor() {
     this.cover = document.querySelector('#cover');
     this.terminalMenu = document.querySelector('i');
     this.intro = document.querySelector('.intro');
+    this.terminal = document.querySelector('.terminal');
+    this.terminalInput = document.querySelector('.terminal-input');
+    this.console = document.querySelector('.console');
 
-    fadeOut(this.cover);
+    this.root = true;
 
     this.attachListeners();
+
+    fadeOut(this.cover);
   }
   attachListeners() {
     this.terminalMenu.onclick = () => {
       this.intro.classList.toggle('clicked');
     }
+    this.terminalInput.onkeypress = (e) => {
+      if ((e.keyCode ? e.keyCode : e.which) === 13) {
+        this.getOutput(this.terminalInput.value);
+      }
+    }
+  }
+  getOutput(value) {
+    this.terminal.removeChild(this.console);
+
+    if (this.root) {
+      this.terminal.appendChild(createEl('P', '', '~ $ashish >> ' + value));
+    } else {
+      this.terminal.appendChild(createEl('P', '', '~/projects $ashish >> ' + value));
+    }
+
+    switch (value) {
+      case 'ls':
+        if (this.root) {
+          this.terminal.appendChild(createEl('P', '', 'projects &nbsp;&nbsp;resume.pdf &nbsp;&nbsp;me.jpg'));
+        } else {
+          this.terminalMenu.appendChild(createEl('P', '', 'Progressively &nbsp;&nbsp;Clippy &nbsp;&nbsp;ink &nbsp;&nbsp;Erectus'));
+        }
+    }
+
+    if (this.root) {
+      this.terminal.appendChild(createEl('P', 'console', '~ $ashish >> <input type="text" class="terminal-input" autofocus>'));
+    } else {
+      this.terminal.appendChild(createEl('P', 'console', '~/projects $ashish >> <input type="text" class="terminal-input" autofocus>'));
+    }
   }
 }
+
 
 window.onload = () => {
   let Term = new Terminal;
