f8fcc934ff5c1fb2e935ffab1ecc2c7c7af1e21f
thinker3197 <ashishgupta.3197@gmail.com>
2018-05-16 17:44:28 +0530

    Add couple more dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./containers/app";
+import registerServiceWorker from "./registerServiceWorker";
+
+import "./base.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
 registerServiceWorker();
