cb5b260784954d4464394361afef25e82169ed4c
Ashish <ashish@spars.in>
2021-07-16 04:24:54 +0530

    Finalize profile

---
+++ js/main.js
@@ -0,0 +1,98 @@
+import anime from "animejs";
+
+// const cursor = document.querySelector("#cursor");
+// const resumeCta = document.querySelector("#resume-cta");
+
+// document.addEventListener("mousemove", (e) => {
+//   cursor.style.left = e.clientX - cursor.offsetWidth / 2 + "px";
+//   cursor.style.top = e.clientY - cursor.offsetHeight / 2 + "px";
+//   cursor.style.opacity = 0.5;
+
+//   if (e.target === resumeCta) {
+//     cursor.style.opacity = 0;
+//   }
+// });
+
+const heading = document.querySelectorAll(".about__content > h1");
+heading.forEach((item) => {
+  item.innerHTML = item.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+});
+
+anime({
+  targets: "#cursor",
+  y: 0,
+});
+
+const animation = anime
+  .timeline({ easing: "easeOutExpo" })
+  .add({
+    targets: ".letter",
+    translateY: [120, 0],
+    delay: anime.stagger(30, { start: 0 }),
+  })
+  .add(
+    {
+      targets: ".about__content > p.description",
+      translateY: [100, 0],
+      opacity: [0, 1],
+    },
+    "-=300"
+  )
+  .add(
+    {
+      targets: ".about__content > p.current",
+      translateY: [100, 0],
+      opacity: [0, 1],
+    },
+    "-=600"
+  )
+  .add(
+    {
+      targets: ".about__resume",
+      opacity: [0, 1],
+    },
+    "-=400"
+  )
+  .add(
+    {
+      targets: ".profile__img",
+      scale: [0, 1],
+    },
+    "-=500"
+  )
+  .add(
+    {
+      targets: ".profile__social-item",
+      opacity: [0, 1],
+    },
+    "-=500"
+  )
+  .add(
+    {
+      targets: ".profile__divider",
+      scale: [0, 1],
+      translateY: [100, 0],
+    },
+    "-=1000"
+  )
+  .add(
+    {
+      targets: ".logo",
+      translateY: [-100, 0],
+    },
+    "-=1000"
+  )
+  .add(
+    {
+      targets: ".profile__backdrop",
+      left: [0, -33],
+      top: [0, 33],
+      opacity: [0, 1],
+    },
+    "-=500"
+  );
+
+animation.finished.then(() => {});
